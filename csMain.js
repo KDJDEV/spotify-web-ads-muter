0d670413277b79d505ca120d076621397cc98696
KDJDEV <kyledagman7@gmail.com>
2023-08-14 15:23:26 -0700

    first commit

---
+++ csMain.js
@@ -0,0 +1,11 @@
+console.info(`%cSpotify Add Muter v${chrome.runtime.getManifest().version} has started!`, "color: orange");
+
+const title = document.querySelector("title");
+
+observer = new MutationObserver(() => {
+    const titleText = title.innerText;
+    const isAdvertisement = titleText.indexOf("Spotify – ") !== -1; // this string in full appears as "Spotify – Advertisement", but the final part can be in different languages, so we just include the first part
+    chrome.runtime.sendMessage({ muted: isAdvertisement });
+});
+
+observer.observe(title, { characterData: false, childList: true, attributes: false });
